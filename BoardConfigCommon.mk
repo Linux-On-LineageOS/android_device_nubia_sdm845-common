211465ddecabe04d298866508a76550d89071b90
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:16:59 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,3 +163,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 include vendor/nubia/sdm845-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
